88b2142e79c4bda73a1d69d859b1a3d31b3086de
github-aliceyang <aliyang47@gmail.com>
2023-05-05 15:59:13 -0700

    created about page

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutPageContent from '../components/AboutPageContent'
+import FooterSection from '../components/FooterSection'
+
+function AboutPage() {
+  return (
+    <div>
+        <AboutPageContent />
+        <FooterSection />
+    </div>
+  )
+}
+
+export default AboutPage
