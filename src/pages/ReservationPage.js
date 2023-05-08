9cee69fa9408a48f57c41de677b9e9be21ed104c
github-aliceyang <aliyang47@gmail.com>
2023-05-08 13:33:58 -0700

    created reservation page

---
+++ src/pages/ReservationPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AvailableTimes from '../components/AvailableTimes'
+
+function ReservationPage() {
+  return (
+      <div> 
+           <AvailableTimes />
+    </div>
+  )
+}
+
+export default ReservationPage
