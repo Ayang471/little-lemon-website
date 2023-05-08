2017a4a95831d58b8c4605fcb2c9097dda3c9fce
github-aliceyang <aliyang47@gmail.com>
2023-05-08 14:07:29 -0700

    created the home page components and added the files

---
+++ src/HomePageComponents/AboutSection.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Chef from '../images/restaurant chef B.png'
+
+
+function AboutSection() {
+  return (
+      <div className='about-section'>
+          <div className='container'>
+              <div className='row'>
+                  <div className='col'>
+                      <div className='res-image'>
+                          <img src={Chef} alt='image3' />
+                      </div>
+                  </div>
+                  <div className='col'>
+                      <div className="about-summary" >
+                          <h1>Little Lemon</h1>
+                          <div className='underline'></div>
+                          <p className='city'>Chicago</p>
+                          <p className='description'>Lorem ipsum dolor sit amet consectetur. Nunc eros amet ut risus blandit urna neque morbi quis.
+                              Tincidunt maecenas sed ornare hendrerit posuere interdum amet nunc. Facilisis bibendum nunc
+                              erat sem. Sed euismod sed varius ac et feugiat. Nibh pretium purus facilisis pellentesque
+                              facilisi dignissim sit in arcu.</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default AboutSection
---
+++ src/HomePageComponents/FooterSection.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import footerImage from '../images/logo1.png'
+import { SocialIcon } from 'react-social-icons';
+import { Link } from 'react-router-dom'
+import { Hide, Box } from '@chakra-ui/react'
+
+function FooterSection() {
+  return (
+      <div className='footer-section'>
+          <div className='container'>
+              <div className='row'>
+                  <div className='col'>
+                      <Hide breakpoint='(max-width: 400px)'>
+                          <Box>
+                              <img src={footerImage} alt='footer-img' />
+                          </Box>
+                      </Hide>
+                  </div>
+                  <div className="col">
+                      <p>Doormat Nagivation</p>
+                      <div><Link to="/home" >Home</Link></div>
+                      <div><Link to="/about" >About</Link></div>
+                      <div><Link to="/menu" >Menu</Link></div>
+                      <div><Link to="/booking" >Reservation</Link></div>
+                      <div><Link to="/online-order" >Online Order</Link></div>
+                      <div><Link to="/login" >LogIn</Link></div>
+                  </div>
+
+                  <div className="col">
+                      <p>Contact</p>
+                      <div><a href='/'>San Jose, CA</a></div>
+                      <div><a href="/">xxx-xxx-xxxx</a></div>
+                      <div><a a href="/">example@Little-Lemon.com</a></div>
+                  </div>
+
+                  <div className='col social-link-icons'>
+                      <Hide breakpoint='(max-width: 400px)'>
+                          <Box>
+                              <p>Social Media</p>
+                              <div className='social-icons'>
+                                  <div className="icons" ><SocialIcon network="twitter" style={{ height: 30, width: 30 }} /></div>
+                                  <div className="icons"><SocialIcon network="instagram" style={{ height: 30, width: 30 }} /></div>
+                                  <div className="icons"><SocialIcon network="facebook" style={{ height: 30, width: 30 }} /></div>
+                              </div>
+                          </Box>
+                      </Hide>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default FooterSection
