5f3f3c1d81d8b7a093157e35e7a2c5eee7163000
github-aliceyang <aliyang47@gmail.com>
2023-05-10 16:16:35 -0700

    Update SpecialsSection.js

--- src/HomePageComponents/SpecialsSection.js
+++ src/HomePageComponents/SpecialsSection.js
@@ -21,118 +21,120 @@ function SpecialsSection() {
                       <span className="orange-wedge-L"></span>
                   </div>
               </div>
-              <div className='row'>
-                  <div className='col'>
-                      <Card className='card'>
-                          <CardActionArea>
-                              <CardMedia
-                                  component="img"
-                                  height="200px"
-                                  width="100%"
-                                  image={greeksalad}
-                                  alt="greek salad"
-                              />
-                              <CardContent className="card-body">
-                                  <Typography >
-                                      <h4>Greek Salad <small>$12.99</small></h4>
-                                      <p>This famous greek salad of crispy lettuce, peppers,
-                                          olives and our Chicago style feta cheese,
-                                          garnished with crunchy garlic and rosemary croutons.</p>
-                                  </Typography>
-                              </CardContent>
-                          </CardActionArea>
-                          <CardActions>
-                              <Button className='order-delivery'>
-                                  <a href="/">
-                                      Order a Delivery
-                                      <span> <FontAwesomeIcon icon={faCar} /></span>
-                                  </a>
-                              </Button>
-                          </CardActions>
-                      </Card>
+              <section>
+                  <div className='row'>
+                      <div className='col'>
+                          <Card className='card'>
+                              <CardActionArea>
+                                  <CardMedia
+                                      component="img"
+                                      height="200px"
+                                      width="100%"
+                                      image={greeksalad}
+                                      alt="greek salad"
+                                  />
+                                  <CardContent className="card-body">
+                                      <Typography >
+                                          <h4>Greek Salad <small>$12.99</small></h4>
+                                          <p>This famous greek salad of crispy lettuce, peppers,
+                                              olives and our Chicago style feta cheese,
+                                              garnished with crunchy garlic and rosemary croutons.</p>
+                                      </Typography>
+                                  </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                  <Button className='order-delivery'>
+                                      <a href="/">
+                                          Order a Delivery
+                                          <span> <FontAwesomeIcon icon={faCar} /></span>
+                                      </a>
+                                  </Button>
+                              </CardActions>
+                          </Card>
+                      </div>
+                      <div className='col'>
+                          <Card className='card'>
+                              <CardActionArea>
+                                  <CardMedia
+                                      component="img"
+                                      height="200px"
+                                      width="100%"
+                                      image={burchetta}
+                                      alt="greek salad"
+                                  />
+                                  <CardContent className="card-body">
+                                      <Typography >
+                                          <h4>Burchetta <small>$5.99</small></h4>
+                                          <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                                      </Typography>
+                                  </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                  <Button className='order-delivery'>
+                                      <a href="/">
+                                          Order a Delivery
+                                          <span> <FontAwesomeIcon icon={faCar} /></span>
+                                      </a>
+                                  </Button>
+                              </CardActions>
+                          </Card>
+                      </div>
+                      <div className='col'>
+                          <Card className='card' >
+                              <CardActionArea>
+                                  <CardMedia
+                                      component="img"
+                                      height="200px"
+                                      width="100%"
+                                      image={lemondessert}
+                                      alt="greek salad"
+                                  />
+                                  <CardContent className="card-body">
+                                      <Typography >
+                                          <h4>Lemon Dessert <small>$5.00</small></h4>
+                                          <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                                      </Typography>
+                                  </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                  <Button className='order-delivery'>
+                                      <a href="/">
+                                          Order a Delivery
+                                          <span> <FontAwesomeIcon icon={faCar} /></span>
+                                      </a>
+                                  </Button>
+                              </CardActions>
+                          </Card>
+                      </div>
+                      <div className='col'>
+                          <Card className='card'>
+                              <CardActionArea>
+                                  <CardMedia
+                                      component="img"
+                                      height="200px"
+                                      width="100%"
+                                      image={lemondessert}
+                                      alt="greek salad"
+                                  />
+                                  <CardContent className="card-body">
+                                      <Typography >
+                                          <h4>Lemon Dessert <small>$5.00</small></h4>
+                                          <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                                      </Typography>
+                                  </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                  <Button className='order-delivery'>
+                                      <a href="/">
+                                          Order a Delivery
+                                          <span> <FontAwesomeIcon icon={faCar} /></span>
+                                      </a>
+                                  </Button>
+                              </CardActions>
+                          </Card>
+                      </div>
                   </div>
-                  <div className='col'>
-                      <Card className='card'>
-                          <CardActionArea>
-                              <CardMedia
-                                  component="img"
-                                  height="200px"
-                                  width="100%"
-                                  image={burchetta}
-                                  alt="greek salad"
-                              />
-                              <CardContent className="card-body">
-                                  <Typography >
-                                      <h4>Burchetta <small>$5.99</small></h4>
-                                      <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                                  </Typography>
-                              </CardContent>
-                          </CardActionArea>
-                          <CardActions>
-                              <Button className='order-delivery'>
-                                  <a href="/">
-                                      Order a Delivery
-                                      <span> <FontAwesomeIcon icon={faCar} /></span>
-                                  </a>
-                              </Button>
-                          </CardActions>
-                      </Card>
-                  </div>
-                  <div className='col'>
-                      <Card className='card' >
-                          <CardActionArea>
-                              <CardMedia
-                                  component="img"
-                                  height="200px"
-                                  width="100%"
-                                  image={lemondessert}
-                                  alt="greek salad"
-                              />
-                              <CardContent className="card-body">
-                                  <Typography >
-                                      <h4>Lemon Dessert <small>$5.00</small></h4>
-                                      <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                                  </Typography>
-                              </CardContent>
-                          </CardActionArea>
-                          <CardActions>
-                              <Button className='order-delivery'>
-                                  <a href="/">
-                                      Order a Delivery
-                                      <span> <FontAwesomeIcon icon={faCar} /></span>
-                                  </a>
-                              </Button>
-                          </CardActions>
-                      </Card>
-                  </div>
-                  <div className='col'>
-                      <Card className='card'>
-                          <CardActionArea>
-                              <CardMedia
-                                  component="img"
-                                  height="200px"
-                                  width="100%"
-                                  image={lemondessert}
-                                  alt="greek salad"
-                              />
-                              <CardContent className="card-body">
-                                  <Typography >
-                                      <h4>Lemon Dessert <small>$5.00</small></h4>
-                                      <p>This famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                                  </Typography>
-                              </CardContent>
-                          </CardActionArea>
-                          <CardActions>
-                              <Button className='order-delivery'>
-                                  <a href="/">
-                                      Order a Delivery
-                                      <span> <FontAwesomeIcon icon={faCar} /></span>
-                                  </a>
-                              </Button>
-                          </CardActions>
-                      </Card>
-                  </div>
-              </div>
+              </section>
           </div>
       </div>
   )
