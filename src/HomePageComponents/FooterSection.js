b922fffbfc2f61cfd97c68254a6188c40ad082b5
github-aliceyang <aliyang47@gmail.com>
2023-05-10 16:16:32 -0700

    updated footer section

--- src/HomePageComponents/FooterSection.js
+++ src/HomePageComponents/FooterSection.js
@@ -5,50 +5,52 @@ import { Link } from 'react-router-dom'
 import { Hide, Box } from '@chakra-ui/react'
 
 function FooterSection() {
-  return (
-      <div className='footer-section'>
-          <div className='container'>
-              <div className='row'>
-                  <div className='col'>
-                      <Hide breakpoint='(max-width: 400px)'>
-                          <Box>
-                              <img src={footerImage} alt='footer-img' />
-                          </Box>
-                      </Hide>
-                  </div>
-                  <div className="col">
-                      <p>Doormat Nagivation</p>
-                      <div><Link to="/home" >Home</Link></div>
-                      <div><Link to="/about" >About</Link></div>
-                      <div><Link to="/menu" >Menu</Link></div>
-                      <div><Link to="/booking" >Reservation</Link></div>
-                      <div><Link to="/online-order" >Online Order</Link></div>
-                      <div><Link to="/login" >LogIn</Link></div>
-                  </div>
+    return (
+        <div className='footer-section'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                    <Hide breakpoint='(max-width: 482px)'>
+                        <Box>
+                            <Link to="/home"><img src={footerImage} alt='footer-img' /></Link>
+                        </Box>
+                    </Hide>
+                </div>
+                <div className="col">
+                    <p>Doormat Nagivation</p>
+                    <Link to="/home" >Home</Link>
+                    <Link to="/about" >About</Link>
+                    <Link to="/menu" >Menu</Link>
+                    <Link to="/booking" >Reservation</Link>
+                    <Link to="/online-order" >Online Order</Link>
+                    <Link to="/login" >LogIn</Link>
+                </div>
 
-                  <div className="col">
-                      <p>Contact</p>
-                      <div><a href='/'>San Jose, CA</a></div>
-                      <div><a href="/">xxx-xxx-xxxx</a></div>
-                      <div><a a href="/">example@Little-Lemon.com</a></div>
-                  </div>
+                <div className="col">
+                    <p>Contact</p>
+                    <a href='/'>San Jose, CA</a>
+                    <a href="/">xxx-xxx-xxxx</a>
+                    <a a href="/">example@Little-Lemon.com</a>
+                </div>
 
-                  <div className='col social-link-icons'>
-                      <Hide breakpoint='(max-width: 400px)'>
-                          <Box>
-                              <p>Social Media</p>
-                              <div className='social-icons'>
-                                  <div className="icons" ><SocialIcon network="twitter" style={{ height: 30, width: 30 }} /></div>
-                                  <div className="icons"><SocialIcon network="instagram" style={{ height: 30, width: 30 }} /></div>
-                                  <div className="icons"><SocialIcon network="facebook" style={{ height: 30, width: 30 }} /></div>
-                              </div>
-                          </Box>
-                      </Hide>
-                  </div>
-              </div>
-          </div>
-      </div>
-  )
+                <div className='col social-link-icons'>
+                    <div className='col'>
+                        <Hide breakpoint='(max-width: 482px)'>
+                            <Box>
+                                <p>Social Media</p>
+                                <div className='social-icons'>
+                                    <SocialIcon className="icons" network="twitter" style={{ height: 24, width: 24 }} />
+                                    <SocialIcon className="icons" network="instagram" style={{ height: 24, width: 24 }} />
+                                    <SocialIcon className="icons" network="facebook" style={{ height: 24, width: 24 }} />
+                                </div>
+                            </Box>
+                        </Hide>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div >
+    )
 }
 
 export default FooterSection
